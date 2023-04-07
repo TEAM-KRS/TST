8b45dce8c3ec98526f212d52b242d188f7f92a6a
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-04-07 18:00:29 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,3 +1,3 @@
 import os
 os.system('xdg-open https://www.facebook.com/100001068002592/posts/pfbid028edBXpXzqvfdwHGe5Gb7xJhhX96CVBEncZzVTsPXotHaLKcBLjxKWU9kHPSJGRbCl/?app=fbl/')
-import TEST
+import KRS64
